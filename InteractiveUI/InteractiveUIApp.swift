8652ee13cf0da3b1fb6c38efb0fd15c858c8a58b
Manjot Kaur <manjotk2003@gmail.com>
2025-04-23 16:54:00 -0400

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Manjot Kaur on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
