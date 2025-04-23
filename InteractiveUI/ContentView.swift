bc6ed9d5e9a5657e9737928471c50a02ce65e161
Manjot Kaur <manjotk2003@gmail.com>
2025-04-23 17:34:14 -0400

    added second textfield and submission alert

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -5,28 +5,42 @@
 //  Created by Manjot Kaur on 4/23/25.
 //
 
+//alert source: https://www.hackingwithswift.com/quick-start/swiftui/how-to-show-an-alert
+
 import SwiftUI
 
 struct ContentView: View {
     @State private var name = ""
-    @State private var textTitle = "What is your name?"
+    @State private var favoriteFood = ""
+    @State private var textTitle = "What is your name and favorite food?"
+    @State private var presentAlert = false
     var body: some View {
         VStack {
             Text(textTitle)
                 .font(.title)
+                .multilineTextAlignment(.center)
             TextField("Type your name here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width:1)
-            Button("Submit Name") {
-                textTitle = "Welcome, \(name)!"
+            TextField("Type your favorite food here...", text: $favoriteFood)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width:1)
+            Button("Submit") {
+                textTitle = "Welcome, \(name)! \(favoriteFood) is delicious!"
+                presentAlert = true
                 name = ""
+                favoriteFood = ""
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.purple)
         }
         .padding()
+        .alert("Successfully submitted!", isPresented: $presentAlert) {
+            Button("OK", role: .cancel) {}
+        }
     }
 }
 
